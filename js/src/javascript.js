cee786a2af87ac7823fb75256d448c883f7b0791
renagadex2 <etan.berkowitz@gmail.com>
2014-03-10 20:44:43 -0400

    hw update

--- js/src/javascript.js
+++ js/src/javascript.js
@@ -1,1 +1,20 @@
-javascript.js
+var userInput = prompt("Choose a category: foods / sports / games / music / movies")
+
+var game = document.querySelector(".new-game");
+game.addEventListener("click", categoryToWord(userInput));
+
+var gameWord = document.querySelector(".game-word");
+gameWord.innerText = userGame.wordGuess;
+
+
+
+var inputText = document.querySelector("#letter");
+inputText.addEventListener("keyup",function(){
+	var letter = inputText.value
+	userGame.guessCorrect(letter)
+	inputText.value = ""
+}); 
+
+
+
+
